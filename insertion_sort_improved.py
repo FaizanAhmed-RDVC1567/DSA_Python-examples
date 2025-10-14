8d1e57eb399d72bc4e546e423da51a8230358fc1
Faizan Ahmed <72260047+FaizanAhmed-RDVC1567@users.noreply.github.com>
2025-10-14 19:09:20 +0100

    Create insertion_sort_improved.py

---
+++ insertion_sort_improved.py
@@ -0,0 +1,20 @@
+def main():
+	my_arr = [64, 32, 85, 7, 29, 10, 44, 16]
+
+	arr_len = len(my_arr)
+	for i in range (1, arr_len):
+		insert_index = i
+		current_value = my_arr[i]
+		for j in range(i-1, -1, -1):
+			if my_arr[j] > current_value:
+				my_arr[j+1] = my_arr[j]
+				insert_index = j
+			else:
+				break
+		my_arr[insert_index] = current_value
+
+	print("Sorted array: ", my_arr)
+
+
+if __name__ == '__main__':
+	main()
